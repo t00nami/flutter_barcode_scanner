f5e2fbaeacedc2c0ffd2ecdecc1574cdff2d6aeb
robertogomezsyon <roberto.gomez.syon@gmail.com>
2025-08-20 14:05:35 +0200

    Update EmbeddingV1Activity.java
    V2

--- example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
+++ example/android/app/src/main/java/com/amolg/flutterbarcodescannerexample/EmbeddingV1Activity.java
@@ -1,14 +1,22 @@
 package com.amolg.flutterbarcodescannerexample;
 
 import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.amolg.flutterbarcodescanner.FlutterBarcodeScannerPlugin;
 
-public class EmbeddingV1Activity extends FlutterActivity {
+public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        // Registrar manualmente el plugin si es necesario
+        flutterEngine.getPlugins().add(new FlutterBarcodeScannerPlugin());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(new FlutterEngine(this));
+        // No es necesario GeneratedPluginRegistrant en embedding V2
     }
 }
